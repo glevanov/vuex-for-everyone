87cc37313256add98f86b1db059389f4a9526e0d
Grigoriy Levanov <glevanov@gmail.com>
2019-03-02 17:09:33 +0300

    Урок по фичам корзины

--- src/store/index.js
+++ src/store/index.js
@@ -7,27 +7,87 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     products: [],
+    cart: [],
+    checkoutStatus: null,
   },
   mutations: {
     setProducts(state, products) {
       state.products = products;
     },
+    pushProductToCart(state, productID) {
+      state.cart.push({
+        id: productID,
+        quantity: 1,
+      });
+    },
+    incrementItemQuantity(state, cartItem) {
+      cartItem.quantity++;
+    },
+    decrementProductInventory(state, product) {
+      product.inventory--;
+    },
+    setCheckoutStatus(state, status) {
+      state.checkoutStatus = status;
+    },
+    emptyCart(state) {
+      state.cart = [];
+    },
   },
   actions: {
     fetchProducts({ commit }) {
-      return new Promise((resolve, reject) => {
+      return new Promise((resolve) => {
         shop.getProducts((products) => {
           commit('setProducts', products);
           resolve();
         });
       });
     },
+    addProductToCart(context, product) {
+      if (product.inventory > 0) {
+        const cartItem = context.state.cart.find(item => item.id === product.id);
+        if (!cartItem) {
+          context.commit('pushProductToCart', product.id);
+        } else {
+          context.commit('incrementItemQuantity', cartItem);
+        }
+        context.commit('decrementProductInventory', product);
+      }
+    },
+    checkout({ state, commit }) {
+      shop.buyProducts(
+        state.cart,
+        () => {
+          commit('emptyCart');
+          commit('setCheckoutStatus', 'success');
+        },
+        () => {
+          commit('setCheckoutStatus', 'fail');
+        },
+      );
+    },
   },
   getters: {
     availableProducts(state) {
       return state.products.filter(
-        product => product.inventory > 0
+        product => product.inventory > 0,
       );
+    },
+    cartProducts(state) {
+      return state.cart.map((cartItem) => {
+        const product = state.products.find(product => product.id === cartItem.id);
+        return {
+          title: product.title,
+          price: product.price,
+          quantity: cartItem.quantity,
+        };
+      });
+    },
+    cartTotal(state, getters) {
+      let total = 0;
+      getters.cartProducts.forEach(product => {
+        total += product.price * product.quantity;
+      });
+      return total;
     },
   },
 });
